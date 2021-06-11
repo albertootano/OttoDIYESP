8be90d5dbe87eb5ea3d2b30511af9c42cbde24a2
albertootano <albertootano1997@gmail.com>
2021-06-11 17:09:21 +0200

    comit alberto

--- ESP32PWM.cpp
+++ ESP32PWM.cpp
@@ -95,7 +95,7 @@ int ESP32PWM::allocatenext(double freq) {
 		return pwmChannel;
 	}
 	Serial.println(
-			"ERROR All PWM timers allocated! Can't accomodate    2 " + String(freq)
+			"ERROR All PWM timers allocated! Can't accomodate    2  3 " + String(freq)
 					+ "Hz\r\nHalting...");
 	while (1)
 		;
